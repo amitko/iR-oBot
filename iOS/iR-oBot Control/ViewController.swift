31dc8f1d02ec368cb1278028cd3ae1c855702288
Maksim Dimitrov <m.dimitrov@digimark.bg>
2017-05-29 13:41:43 +0300

    Added iOS Prototype

---
+++ iOS/iR-oBot Control/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  iR-oBot Control
+//
+//  Created by Maksim Dimitrov on 5/29/17.
+//  Copyright © 2017 Maksim Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cameraView.layer.shadowColor = UIColor.black.cgColor
+        cameraView.layer.shadowOffset = CGSize(width: 1, height: 1)
+        cameraView.layer.shadowOpacity = 0.2
+        cameraView.layer.borderWidth = 1
+        cameraView.layer.borderColor = UIColor.darkGray.cgColor
+        
+    }
+
+    
+    @IBAction func throttleChanged(_ sender: UISlider) {
+        print("Throttle Value: \t\(sender.value)")
+    }
+    
+    @IBAction func armPositionChanged(_ sender: UISlider) {
+        print("Arm Position Value: \t\(sender.value)")
+    }
+    
+    @IBAction func rotationChanged(_ sender: UISlider) {
+        print("Rotation Value: \t\(sender.value)")
+    }
+    
+    @IBAction func rotationSliderReleased(_ sender: UISlider) {
+        UIView.animate(withDuration: 0.2) {
+            sender.setValue(0, animated: true)
+            sender.sendActions(for: .valueChanged)
+        }
+    }
+    
+}
+
